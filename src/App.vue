82dfa15621d94dff0c04786e9c1e112d1927f52d
Dexaas <Leonardo1236@op.pl>
2023-03-05 22:14:20 +0100

    working buttons

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <ButtonCompiler />
+  </div>
 </template>
 
+<script>
+import ButtonCompiler from "./components/ButtonCompiler.vue";
 
-
-
-
-
-
-
-
+export default {
+  components: {
+    ButtonCompiler
+}
+};
+</script>
+<!--
 <style scoped>
 header {
   line-height: 1.5;
@@ -91,3 +77,4 @@ nav a:first-of-type {
   }
 }
 </style>
+-->
---
+++ src/components/ButtonCompiler.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="row in 7" :key="row">
+          <td v-for="(col, index) in colors" :key="index">
+            <button class="button1">{{ row }} {{ col }}</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import bridgeOpenings from './bridgeOpenings.json';
+
+export default {
+  data() {
+    return {
+      colors: ["♣", "♦", "♥", "♠", "NT"],
+      bridgeOpenings: bridgeOpenings.bridgeOpenings
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+  .button1{
+    background-color: hsla(160, 100%, 37%, 0.2);
+    color: aliceblue;
+    height: 30px;
+    width: 100px;
+    cursor: pointer;
+    text-align: center; 
+    border: none;
+    border-radius: 6px;
+}
+  .button1:hover{
+    background-color: hsla(160, 100%, 37%, 1);
+    color: white;
+    transition: 0.7s;
+    border-radius: 12px;
+  }
+</style>
